4eeb9b4638c09867ea1543fa1f99185ef26b1a0e
wesley956 <aweservicosaw@gmail.com>
2026-06-03 23:32:57 +0000

    Add PWA install support

---
+++ public/sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'pm-sp-2026-arena-v1';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
+        '/',
+        '/manifest.webmanifest',
+        '/icon-192.svg',
+        '/icon-512.svg'
+      ])
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request).then(cached =>
+          cached || caches.match('/')
+        )
+      )
+  );
+});
